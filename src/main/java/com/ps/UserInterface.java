390ff0bb0d2368772e2ff933e039d7d81af0e833
Christian <you@example.com>
2024-10-25 11:55:55 -0500

    phase 1 complete

---
+++ src/main/java/com/ps/UserInterface.java
@@ -0,0 +1,15 @@
+package com.ps;
+
+public class UserInterface {
+    // private static Dealership dealership;
+
+    public static void init(){
+        // init method
+        //loading the dealership and inventory from a file
+    }
+
+    public static void display (){
+        init();
+        // load the menu
+    }
+}
